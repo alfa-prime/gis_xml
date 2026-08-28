bd4d7c8a979b9b420ce7e8f5ba5a6241ac2769a3
devchoco <uglin76@mail.ru>
2026-08-28 12:58:21 +0300

    добавлен health router

--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ from app.router import health_router
 tags_metadata = []
 
 app = FastAPI(
-    tags = tags_metadata,
-    root_path = "/gis_oms",
-    title = "GIS-OMS",
-    description = "GIS-OMS",
-    version = "0.0.1",
+    tags=tags_metadata,
+    root_path="/gis_oms",
+    title="GIS-OMS",
+    description="GIS-OMS",
+    version="0.0.1",
 )
 
 
