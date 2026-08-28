df6665a29c508e5cf3f848a02b19c339ca86e61c
Alexsander Uglin <uglin76@mail.ru>
2026-08-28 16:18:01 +0300

    chore: удалены тестовые маршруты обработки ошибок

--- app/router/health.py
+++ app/router/health.py
@@ -2,23 +2,17 @@ from fastapi import APIRouter, Security
 
 from app.core.dependencies import verify_api_key
 
-router = APIRouter(prefix="/health", tags=["Health"], dependencies=[Security(verify_api_key)])
+router = APIRouter(
+    prefix="/health",
+    tags=["Health"],
+    dependencies=[Security(verify_api_key)],
+)
 
 
 @router.get(
     path="/",
     summary="Стандартная проверка работоспособности",
-    description="Возвращает 'pong', если сервис запущен и отвечает на запросы."
+    description="Возвращает 'pong', если сервис запущен и отвечает на запросы.",
 )
 async def ping():
     return {"ping": "pong"}
-
-
-@router.get("/validate")
-async def validate(value: int):
-    return {"value": value}
-
-@router.get("/error")
-async def error():
-    raise RuntimeError("Тестовая ошибка")
-
